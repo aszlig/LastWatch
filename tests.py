250fa8f25459bb68a3e39becc95337dfd3097d60
aszlig <aszlig@redmoonstudios.org>
2010-11-23 06:17:04 +0100

    Rewrote doctests into unittest, testing stuff that makes more sense ;-)

--- tests.py
+++ tests.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python
-import doctest
-from lastwatch import Songinfo
+import unittest
+import lastwatch
 
-__test__ = {'SONGINFO': """
->>> Songinfo('/blah/foo/Moonspell/The Antidote/01. The Antidote.ogg').get_from_fname('title')
-'The Antidote'
->>> Songinfo('/blah/foo/Moonspell - The Antidote.ogg').get_from_fname('artist')
-'Moonspell'
->>> foo = Songinfo('/blah/foo/Moonspell - The Antidote - Blah (foo bar).ogg')
->>> foo.get_from_fname('title')
-'Blah (foo bar)'
->>> foo.get_from_fname('album')
-'The Antidote'
-""", 'RECURSE_SONGINFO': """
->>> import os
->>> for root, dirs, files in os.walk('/home/aszlig/music'):
-...     results = [os.path.join(root, f) for f in files]
-...     for r in results:
-...         if os.path.splitext(r)[1][1:].lower() not in ('mp3', 'ogg', 'flac'): continue
-...         foo = Songinfo(r)
-...         assert {'artist': foo.get_from_fname('artist')}
-...         assert {'title': foo.get_from_fname('title')}
->>>
-"""}
+class FilenameTest(unittest.TestCase):
+    def parse_and_check(self, path, expected_result):
+        parser = lastwatch.FilenameParser(path)
+        self.assertEqual(parser.parse(), expected_result)
+
+    def test_filename1(self):
+        path = '/music/Moonspell/The Antidote/01. The Antidote.ogg'
+
+        result = {
+            'album': 'The Antidote',
+            'title': 'The Antidote',
+            'number': '01',
+            'artist': 'Moonspell',
+        }
+
+        self.parse_and_check(path, result)
+
+    def test_filename2(self):
+        path = '/music/Linkin Park - A Thousand Suns (2010)/'
+        path += '08. Linkin Park - Waiting for the End.ogg'
+
+        result = {
+            'album': 'A Thousand Suns',
+            'title': 'Waiting for the End',
+            'number': '08',
+            'artist': 'Linkin Park',
+        }
+
+        self.parse_and_check(path, result)
 
 if __name__ == '__main__':
-	doctest.testmod()
+    unittest.main()
